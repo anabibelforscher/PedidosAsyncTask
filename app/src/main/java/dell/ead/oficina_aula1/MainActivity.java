0c9721589d473aff37f15508b6413bfb50518e2d
Ana <anaolivercontact@gmail.com>
2022-01-30 15:36:48 -0300

    Layout activity_main.xml

--- app/src/main/java/dell/ead/oficina_aula1/MainActivity.java
+++ app/src/main/java/dell/ead/oficina_aula1/MainActivity.java
@@ -2,12 +2,21 @@ package dell.ead.oficina_aula1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButtonAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonAction = findViewById(R.id.buttonAction);
+    }
+
+    public void setButton (String message){
+      mButtonAction.setText(message);
     }
 }

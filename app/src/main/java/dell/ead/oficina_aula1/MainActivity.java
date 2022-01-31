cbfd7a5eab2e653a81e508209eb0ab7c7c885fbd
Ana <anaolivercontact@gmail.com>
2022-01-30 22:34:05 -0300

    Layout lista_pedidos.xml

--- app/src/main/java/dell/ead/oficina_aula1/MainActivity.java
+++ app/src/main/java/dell/ead/oficina_aula1/MainActivity.java
@@ -3,10 +3,12 @@ package dell.ead.oficina_aula1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button mButtonAction;
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void setButton (String message){
       mButtonAction.setText(message);
+    }
+
+    public void setImageViewUpdate (int imageView){
+        mImageView = findViewById(R.id.imageOrder);
+        mImageView.setImageResource(imageView);
     }
 }

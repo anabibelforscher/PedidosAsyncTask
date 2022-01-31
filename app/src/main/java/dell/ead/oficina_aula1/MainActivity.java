2dd570c064291694a9b7cc64d2637f05499ae130
Ana <anaolivercontact@gmail.com>
2022-01-31 15:59:46 -0300

    Configuração ListView

--- app/src/main/java/dell/ead/oficina_aula1/MainActivity.java
+++ app/src/main/java/dell/ead/oficina_aula1/MainActivity.java
@@ -2,13 +2,20 @@ package dell.ead.oficina_aula1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mButtonAction;
-    ImageView mImageView;
+    private Button mButtonAction;
+    private ListView mListView;
+    private String[] mOrders = {
+            "Teste", "teste"
+    };
+    ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +23,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButtonAction = findViewById(R.id.buttonAction);
+        mListView = findViewById(R.id.listViewOrder);
+        setButton("Atualizar Pedidos");
+
+       mAdapter = new ArrayAdapter<>(
+                getApplicationContext(),
+                R.layout.lista_pedidos,
+                R.id.order,
+                mOrders
+        );
     }
 
     public void setButton (String message){
       mButtonAction.setText(message);
+      mButtonAction.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              mListView.setAdapter(mAdapter);
+          }
+      });
     }
 
-    public void setImageViewUpdate (int imageView){
-        mImageView = findViewById(R.id.imageOrder);
-        mImageView.setImageResource(imageView);
-    }
 }
